97160fca3801092b563257f5cdb3bf9ef7f8f441
tahawy111 <amertahawy111@gmail.com>
2023-04-09 02:36:56 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import { db } from '@/lib/db';
 
-const inter = Inter({ subsets: ['latin'] });
-
- export default async function Home() {
+export default async function Home() {
 
   await db.set("Hello","World")
 
